0079d3a099b621ad0e3ad8a13191cd7df6ee5473
garcesgustavo <garcesgustavo@gmail.com>
2022-05-18 08:29:38 -0300

    ccambios_menores

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 var items = ["Buy food", "Cook food", "Eat food"];
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 app.get("/", function (req, res) {
   var today = new Date();
   var options = {
     weekday: "long",
     day: "numeric",
-    month: "long"
+    month: "long",
   };
   var day = today.toLocaleDateString("es-ES", options);
 
   res.render("list", {
     kindOfDay: day,
-    addItem: items
+    addItem: items,
   });
 });
 
@@ -29,6 +31,5 @@ app.post("/", function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log("Server is on port 3000")
-
+  console.log("Server is on port 3000");
 });
